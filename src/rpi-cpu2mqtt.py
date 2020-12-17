da1c95b184e375aa8e66f445a2504885a620e80f
Hristo Jelev <you@example.com>
2020-12-17 19:57:38 +0200

    update comments

--- src/rpi-cpu2mqtt.py
+++ src/rpi-cpu2mqtt.py
@@ -1,4 +1,4 @@
-# Python 2 script to check cpu load, cpu temperature and free space,
+# Python (runs on 2 and 3) script to check cpu load, cpu temperature and free space,
 # on a Raspberry Pi computer and publish the data to a MQTT server.
 # RUN pip install paho-mqtt
 # RUN sudo apt-get install python-pip
