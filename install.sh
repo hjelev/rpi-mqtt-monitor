492dab8fbd4b4d206e60aae10b1ff5f86932958c
hjelev <jeleff@gmail.com>
2022-03-20 20:53:23 +0200

    update

--- install.sh
+++ install.sh
@@ -10,8 +10,10 @@ printm(){
 find_python(){
 if $(python --version); then 
 	python=$(which python)
+	pip="pip"
 else
 	python=$(which python3)
+	pip="pip3"
 fi
 echo "$python"
 }
@@ -32,7 +34,7 @@ check_and_install_pip(){
   pip=$(${python} -m pip --version 2>&1);
   if [[ "$pip" == *"No"* ]]; then
     echo "- Pip is not installed, installing it."
-    sudo apt install python-pip
+    sudo apt install python-$pip
     else
     print_green "+ Found $pip"
   fi
