c47e069df96149ab9f9b5cfcb0c097a26fe6a59b
Hristo <jeleff@gmail.com>
2024-02-01 16:48:40 +0200

    hotfix update system one more time

--- src/update.py
+++ src/update.py
@@ -3,7 +3,6 @@ import os
 import subprocess
 import config
 
-
 def get_assignments(filename):
     with open(filename) as f:
         tree = ast.parse(f.read(), filename)
@@ -39,6 +38,13 @@ def display_config_differences(current_config, example_config, display=True):
     else:
         return False
 
+def check_git_version_remote(script_dir):
+    full_cmd = "git -C {} ls-remote --tags origin | awk -F'/' '{{print $3}}' | sort -V | tail -n 1".format(script_dir)
+    result = subprocess.Popen(full_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0].decode("utf-8")
+    latest_tag = result.strip()
+    return latest_tag if latest_tag else None
+
+
 def update_config_version(version, script_dir):
     with open(script_dir + '/config.py', 'r') as f:
         lines = f.readlines()
@@ -52,11 +58,10 @@ def update_config_version(version, script_dir):
                 f.write(line)
 
 
-def do_update(version=config.version, git_update=True, config_update=True):
-    script_dir = os.path.dirname(os.path.realpath(__file__))
+def do_update(script_dir, version=config.version, git_update=True, config_update=True):
     print("Current version: {}".format(config.version))
     if git_update:
-        print(":: Updating git repository")
+        print(":: Updating git repository", script_dir)
         result = subprocess.run(['git', '-C', script_dir, 'pull'], check=True, universal_newlines=True, stdout=subprocess.PIPE)
         print(result.stdout)
 
@@ -68,5 +73,6 @@ def do_update(version=config.version, git_update=True, config_update=True):
         update_config_version(version, script_dir)
 
 
-if __name__ == '__main__':   
-    do_update()
+if __name__ == '__main__':
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    do_update(script_dir,check_git_version_remote(script_dir))
