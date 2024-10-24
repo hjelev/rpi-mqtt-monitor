02bc51d6a4aa5cb5146d24e21dc5037ee9936af1
Hristo <jeleff@gmail.com>
2024-10-25 00:38:23 +0300

    apt updates sensor

--- src/rpi-cpu2mqtt.py
+++ src/rpi-cpu2mqtt.py
@@ -218,6 +218,20 @@ def get_mac_address():
     return mac
 
 
+def get_pending_updates():
+    try:
+        subprocess.run(['sudo', 'apt', 'update'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        full_cmd = "apt-get -q -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade | /bin/grep ^Inst | wc -l"
+        result = subprocess.run(full_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        
+        num_updates = int(result.stdout.strip())
+    except Exception as e:
+        print(f"Error checking for updates: {e}")
+        num_updates = 0
+
+    return num_updates
+
+
 def get_hwmon_device_name(hwmon_path):
     try:
         with open(os.path.join(hwmon_path, 'name'), 'r') as f:
@@ -254,8 +268,7 @@ def check_all_drive_temps():
 def print_measured_values(cpu_load=0, cpu_temp=0, used_space=0, voltage=0, sys_clock_speed=0, swap=0, memory=0,
                           uptime_days=0, uptime_seconds=0, wifi_signal=0, wifi_signal_dbm=0, rpi5_fan_speed=0, drive_temps=0, rpi_power_status=0):
     remote_version = update.check_git_version_remote(script_dir)
-    output = """
-:: rpi-mqtt-monitor
+    output = """:: rpi-mqtt-monitor
    Version: {}
 
 :: Device Information
